5a10451b2af8ac591a962ee8267a3bffa50ddd23
Yash Shambharkar <yyyyash123@gmail.com>
2026-07-06 00:27:49 +0530

    added trade log and equity curve , equity curve is value of portfolio on each day

--- backtester/backtester.py
+++ backtester/backtester.py
@@ -4,6 +4,9 @@ class Backtester:
         self.initial_cash = initial_cash
 
     def run(self, df, signals) -> dict:
+        trade_log = []
+        equity_curve = []
+
         cash = self.initial_cash
         shares = 0.0
         holding_stock = False
@@ -18,26 +21,42 @@ class Backtester:
                     cash=0
                     holding_stock = True
                     num_trades += 1
+                    buy_price = current_price
+                    buy_date = df.index[i]
 
             elif current_signal == -1 and holding_stock:
-                cash = shares*current_price
+                sold_shares=shares
+                cash = sold_shares*current_price
                 shares=0
                 holding_stock = False
+                trade_log.append({
+                    "buy_date": buy_date,
+                    "buy_price": buy_price,
+                    "sell_date": df.index[i],
+                    "sell_price": current_price,
+                    "shares": sold_shares,
+                    "profit": (current_price - buy_price)*shares
+                })
+
+            portfolio_value = cash + shares*current_price
+            equity_curve.append(portfolio_value)
                   
         last_price = df["Close"].iloc[-1]
         if holding_stock:
-            final_portfolio = cash + shares*df["Close"].iloc[-1]
+            final_portfolio = cash + shares*last_price
 
         else:
            final_portfolio = cash
 
         total_return = (
-            final_portfolio-self.initial_cash) / self.initial_cash * 100
+        final_portfolio-self.initial_cash) / self.initial_cash * 100
             
         return {
             "final_portfolio": final_portfolio,
             "total_return": total_return,
-            "num_trades": num_trades
+            "num_trades": num_trades,
+            "trade_log": trade_log,
+            "equity_curve": equity_curve
         }
     
 
